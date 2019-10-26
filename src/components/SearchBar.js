4cd7a6595d17217775148285159d78d359b6fcf6
Jared Rodriguez <jaredarodriguez@gmail.com>
2019-10-25 19:38:55 -0500

    youtube fetch request made

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,13 +6,19 @@ class SearchBar extends React.Component {
 
     }
 
-    onInputChange = () => {
-
+    onInputChange = event => {
+        this.setState({ term: event.target.value });
     }
+
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        this.props.onFormSubmit(this.state.term);
+    };
     render() {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Video Search</label>
                         <input
